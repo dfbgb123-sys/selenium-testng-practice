7c097ffdb4eb8f1bfe504b36c84478d8928e4bee
dfbgb123-sys <dfbgb123@gmail.com>
2026-03-09 23:58:37 +0900

    Selenium + TestNG 자동화 프로젝트 기본 구조 초기화

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,52 +1,22 @@
 package tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import base.BaseTest;
+import org.example.pages.LoginPage;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTest {
-
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-
-        // ⭐ 드라이버 자동 관리, 다만 반복 시, PC 느려지고 문제 생김 > driver.quit()추가 필요
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-        driver.get("https://the-internet.herokuapp.com/login");
-    }
+public class LoginTest extends BaseTest {
 
     @Test
     public void loginSuccessTest() {
-        System.out.println("Selenium test started"); //나중에 지우기
-        driver.findElement(By.id("username"))
-                .sendKeys("tomsmith");
+        driver.get("https://the-internet.herokuapp.com/login");
 
-        driver.findElement(By.id("password"))
-                .sendKeys("SuperSecretPassword!");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginAs("tomsmith", "SuperSecretPassword!");
+        loginPage.clickLogin();
 
-        driver.findElement(By.cssSelector("button[type='submit']"))
-                .click();
-
-        String successMessage =
-                driver.findElement(By.id("flash")).getText();
-
-        Assert.assertTrue(
-                successMessage.contains("You logged into a secure area!"),
-                "로그인 성공 메시지가 나타나지 않았습니다."
-        );
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
+        String successMessage = loginPage.getFlashMessage();
+        Assert.assertTrue(successMessage.contains("You logged into a secure area!"),
+                "로그인 성공 메시지가 나타나지 않았습니다.");
     }
 }
