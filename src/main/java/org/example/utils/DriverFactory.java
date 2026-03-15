100d4e975044896a5506c660eb8c21ebd9ab740a
dfbgb123-sys <dfbgb123@gmail.com>
2026-03-15 17:43:21 +0900

    DriverFactory수정

--- src/main/java/org/example/utils/DriverFactory.java
+++ src/main/java/org/example/utils/DriverFactory.java
@@ -2,13 +2,42 @@ package org.example.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverFactory {
 
-    public static WebDriver createDriver(){
-        //브라우저 생성 담당
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+//    public static WebDriver createDriver(){
+//        //브라우저 생성 담당
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        return driver;
+//    }
+    private static WebDriver driver;
+
+    public static WebDriver createDriver(String browser){
+
+        if(browser.equalsIgnoreCase("chrome")){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+
+            driver = new ChromeDriver(options);
+            return driver;
+        }
+        else if(browser.equalsIgnoreCase("edge")){
+            driver = new EdgeDriver();
+            driver.manage().window().maximize();
+            return driver;
+        }
+        return null;
+    }
+    public static WebDriver getDriver(){
         return driver;
+    }
+    public static void quitDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
     }
 }
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,22 +2,19 @@ package base;
 
 import org.example.utils.DriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-    //테스트 시작 → driver 생성
-    //테스트 종료 → driver 종료
     protected WebDriver driver;
 
     @BeforeMethod
     public void setup(){
-        driver = DriverFactory.createDriver();
+        driver = DriverFactory.createDriver("chrome");
     }
     @AfterMethod
     public void teardown(){
-        if(driver != null){
-            driver.quit();
-        }
+        DriverFactory.quitDriver();
     }
 }
